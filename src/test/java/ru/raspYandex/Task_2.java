6da486b4e8fa80a7939d76f69fd95dc087d1c340
DatsukMaxim <48429319+DatsukMaxim@users.noreply.github.com>
2019-06-19 21:33:16 +0700

    fix

--- src/test/java/ru/raspYandex/Task_2.java
+++ src/test/java/ru/raspYandex/Task_2.java
@@ -20,7 +20,7 @@ public class Task_2 {
     @Test
     public void test() {
         driver.manage().window().maximize();
-  /*      driver.navigate().to("https://rasp.yandex.ru");
+        driver.navigate().to("https://rasp.yandex.ru");
 
         WebElement fromField = driver.findElement(By.id("from"));
         fromField.sendKeys("Кемерово проспект Ленина");
@@ -36,8 +36,9 @@ public class Task_2 {
 
         WebElement searchButton = driver.findElement(By.xpath("//form[@class='SearchForm__holder']//button[@class='Button SearchForm__submit']"));
         searchButton.click();
-*/
-        driver.navigate().to("https://rasp.yandex.ru/search/bus/?fromName=%D0%9A%D0%B5%D0%BC%D0%B5%D1%80%D0%BE%D0%B2%D0%BE+%D0%BF%D1%80%D0%BE%D1%81%D0%BF%D0%B5%D0%BA%D1%82+%D0%9B%D0%B5%D0%BD%D0%B8%D0%BD%D0%B0&toName=%D0%9A%D0%B5%D0%BC%D0%B5%D1%80%D0%BE%D0%B2%D0%BE+%D0%91%D0%B0%D0%BA%D0%B8%D0%BD%D1%81%D0%BA%D0%B8%D0%B9+%D0%BF%D0%B5%D1%80%D0%B5%D1%83%D0%BB%D0%BE%D0%BA&when=26+%D0%B8%D1%8E%D0%BD%D1%8F");
+
+        //driver.navigate().to("https://rasp.yandex.ru/search/bus/?fromName=%D0%9A%D0%B5%D0%BC%D0%B5%D1%80%D0%BE%D0%B2%D0%BE+%D0%BF%D1%80%D0%BE%D1%81%D0%BF%D0%B5%D0%BA%D1%82+%D0%9B%D0%B5%D0%BD%D0%B8%D0%BD%D0%B0&toName=%D0%9A%D0%B5%D0%BC%D0%B5%D1%80%D0%BE%D0%B2%D0%BE+%D0%91%D0%B0%D0%BA%D0%B8%D0%BD%D1%81%D0%BA%D0%B8%D0%B9+%D0%BF%D0%B5%D1%80%D0%B5%D1%83%D0%BB%D0%BE%D0%BA&when=26+%D0%B8%D1%8E%D0%BD%D1%8F");
+
         boolean text;
         text = driver.findElements(By.linkText("Пункт прибытия не найден. Проверьте правильность написания или выберите другой город.")).size() > 0;
         System.out.println(text);
@@ -45,6 +46,6 @@ public class Task_2 {
 
     @After
     public void close() {
-       // driver.quit();
+       driver.quit();
     }
 }
