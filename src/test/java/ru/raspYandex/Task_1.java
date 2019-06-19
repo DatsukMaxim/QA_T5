3ca501415cfa7cbee2cf01798f97b81442d414b0
DatsukMaxim <48429319+DatsukMaxim@users.noreply.github.com>
2019-06-19 20:31:06 +0700

    changed path to chromedriver

--- src/test/java/ru/raspYandex/Task_1.java
+++ src/test/java/ru/raspYandex/Task_1.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TestTask {
+public class Task_1 {
 
 
     public WebDriver driver;
